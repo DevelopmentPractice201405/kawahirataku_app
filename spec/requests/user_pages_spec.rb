748659e60c87735deeffe22dfc6f11570cc3e5dd
takuyakawahira <takuya_k-12@stu.kbc.ac.jp>
2014-08-14 12:33:55 +0900

    Finish sign in

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -1,54 +1,48 @@
 require 'spec_helper'
 
-describe "User pages" do
+describe "Authentication" do
 
   subject { page }
-  
-  describe "profile page" do
-    let(:user) { FactoryGirl.create(:user) }
-    before { visit user_path(user) }
 
-    it { should have_selector('h1',    text: user.name) }
-    it { should have_selector('title', text: user.name) }
+  describe "signin page" do
+    before { visit signin_path }
+
+    it { should have_selector('h1',    text: 'Sign in') }
+    it { should have_selector('title', text: 'Sign in') }
   end
 
-  describe "signup page" do
-    before { visit signup_path }
-
-    it { should have_selector('h1',    text: 'Sign up') }
-    it { should have_selector('title', text: 'Sign up') }
-  end
-
-  describe "signup" do
-
-    before { visit signup_path }
-
-    let(:submit) { "Create my account" }
+  describe "signin" do
+    before { visit signin_path }
 
     describe "with invalid information" do
-      it "should not create a user" do
-        expect { click_button submit }.not_to change(User, :count)
-      end
+      before { click_button "Sign in" }
+
+      it { should have_selector('title', text: 'Sign in') }
+      it { should have_selector('div.alert.alert-error', text: 'Invalid') }
+
+      describe "after visiting another page" do
+        before { click_link "Home" }
+        it { should_not have_selector('div.alert.alert-error') }
     end
-
-    describe "with valid information" do
+  end
+  describe "with valid information" do
+      let(:user) { FactoryGirl.create(:user) }
       before do
-        fill_in "Name",         with: "Example User"
-        fill_in "Email",        with: "user@example.com"
-        fill_in "Password",     with: "foobar"
-        fill_in "Confirmation", with: "foobar"
+        fill_in "Email",    with: user.email.upcase
+        fill_in "Password", with: user.password
+        click_button "Sign in"
       end
 
-      describe "after saving the user" do
-          before { click_button submit }
-          let(:user) { User.find_by_email('user@example.com') }
+      it { should have_selector('title', text: user.name) }
+      it { should have_link('Profile', href: user_path(user)) }
+      it { should have_link('Sign out', href: signout_path) }
+      it { should_not have_link('Sign in', href: signin_path) }
 
-          it { should have_selector('title', text: user.name) }
-          it { should have_selector('div.alert.alert-success', text: 'Welcome') }
-
-        
-        it { should have_link('Sign out') }
+ describe "followed by signout" do
+        before { click_link "Sign out" }
+        it { should have_link('Sign in') }
       end
     end
   end
 end
+  
